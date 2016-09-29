647c3263b6af93b93ee828c971182d45a602a676
Stav Yardeni <stardeni36@gmail.com>
2016-09-29 15:31:43 +0300

    changes in preprocessing - compounds and mwe

--- eng_conversion/preprocess_eng_childes.py
+++ eng_conversion/preprocess_eng_childes.py
@@ -36,88 +36,65 @@ def to_conll(morphdep, debug=False):
     Receives a sentence in morphdep format, return CoNLL style.
     """
 
-    def fix_offsets(output, added_indices):
+    def fix_offsets(output, fixed_set, index_map):
         """
         Fixes the head indices, given the added indices.
+        1. go over all words execpt those with indices in fixed_set
+        2. change their head_index to index_map[head_index]
         """
-        fixed_set = set()
-        for added in added_indices:
-            fixed_set.update([added[0] + c for c in range(added[1] + 1)])
-            fixed_set.remove(added[0] + added[2])
-        for added in added_indices:
-            for ind in range(len(output)):
-                if output[ind][0] in fixed_set:
-                    continue
-                if output[ind][6] > added[0]:
-                    output[ind][6] += added[1]
-                elif output[ind][6] == added[0]:
-                    output[ind][6] += added[2]
+        for word in output:
+            if word[0] in fixed_set:
+                continue
+            else:
+                word[6]=index_map[word[6]]
+
         return output
 
     output = []
     offset = 1
     added_indices = []
-    for index, word in enumerate(morphdep):
-        if None in word:
+    fixed_set = set()
+    index_map = dict()
+    for word_index in range(len(morphdep)):
+        if None in morphdep[word_index]:
             return None
+        index_map[morphdep[word_index][3]]=morphdep[word_index][3]
+    for index, word in enumerate(morphdep):
         pos = word[1]
         if pos in ['+...', '+/.']:
             return None
-        elif pos in ['prep:pro', 'acc:pro', 'acc:det']:
-            word1 = word[0].split('&')[0]
-            word2 = '&'.join(word[0].split('&')[1:])
-            if word2 == "":
-                word2 = "empty_pronoun"
-            if pos == 'acc:pro':
-                pos1 = 'acc'
-                pos2 = 'pro'
-                dep1 = 'APREP'
-            elif 'prep:pro':
-                pos1 = 'prep'
-                pos2 = 'pro'
-                dep1 = 'APREP'
-            elif pos == 'acc:det':
-                pos1 = 'acc'
-                pos2 = 'det'
-                dep1 = 'APREP'
-            output.append([index + offset, word1, word1, pos1, pos1, '_', word[3], word[4], '_', '_'])
-            output.append([index + offset + 1, word2, word2, pos2, pos2, '_', index + offset, dep1, '_', '_'])
-            added_indices.append((index + offset, 1,
-                                  0))  # (original token number after which the addition occured, how many tokens were added, which of them is the head (0,1,2 etc.))
-            offset += 1
-        elif pos == 'n:det':  # a compound with a determiner
-            sub_words = word[0].split('+')
-            if len(sub_words) != 3 or sub_words[1] != 'ha':
-                sys.stderr.write('Unencountered compound case\n')
-                pdb.set_trace()
-            else:
-                output.append([index + offset, sub_words[0], sub_words[0], 'n', 'n', '_', word[3], word[4], '_', '_'])
-                output.append(
-                    [index + offset + 1, sub_words[1], sub_words[1], 'det', 'det', '_', index + offset + 2, 'det', '_',
-                     '_'])
-                output.append(
-                    [index + offset + 2, sub_words[2], sub_words[2], 'n', 'n', '_', index + offset, 'nmod', '_', '_'])
-                added_indices.append((index + offset, 2, 0))
-                offset += 2
         elif '+' in word[0]:  # noun compound
             sub_words = word[0].split('+')
-            output.append([index + offset, sub_words[0], sub_words[0], 'n', 'n', '_', \
+            last_word=sub_words[-1]
+            position_last_word=index+offset+len(sub_words)-1
+            for c, w in enumerate(sub_words[0:-1]):
+                output.append([index + offset + c, w, w, 'n', 'n', '_', position_last_word, 'compound', '_', '_'])
+            output.append([position_last_word, last_word, last_word, 'n', 'n', '_', \
                            word[3], word[4], '_', '_'])
-            for c, w in enumerate(sub_words[1:]):
-                output.append([index + offset + c + 1, w, w, 'n', 'n', '_', index + offset, 'compound', '_', '_'])
-            added_indices.append((index + offset, len(sub_words) - 1, 0))
+
+            # index_map -- 1. all indices greater than index+offset should be added len(sub_words), 2. change index+offset to position_last_word
+            for key in index_map:
+                if index_map[key]>index+offset:
+                    index_map[key]=index_map[key]+len(sub_words)-1
+                elif index_map[key]==index+offset:
+                    index_map[key]=position_last_word
+            fixed_set.update(list(range(index+offset,position_last_word)))
             offset += len(sub_words) - 1
         elif '_' in word[0]:  # MWE
             sub_words = word[0].split('_')
             output.append([index + offset, sub_words[0], sub_words[0], 'X', 'X', '_', word[3], word[4], '_', '_'])
             for c, w in enumerate(sub_words[1:]):
                 output.append([index + offset + c + 1, w, w, 'X', 'X', '_', index + offset, 'mwe', '_', '_'])
-            added_indices.append((index + offset, len(sub_words) - 1, 0))
+            # same, but here we don't need to change the head index+offset
+            for key in index_map:
+                if index_map[key]>index+offset:
+                    index_map[key]=index_map[key]+len(sub_words)-1
+            fixed_set.update(list(range(index+offset+1,index+offset+len(sub_words))))
             offset += len(sub_words) - 1
         else:
             output.append([index + offset, word[0], word[0], pos, pos, '_', word[3], word[4], '_', '_'])
 
-    output = fix_offsets(output, added_indices)
+    output = fix_offsets(output, fixed_set, index_map)
     return output
 
 
